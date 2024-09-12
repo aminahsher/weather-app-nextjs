05c94fa0a92192b19bcecf299d5846bd89fbf5fa
Aminah Ali <aania2009@gmail.com>
2024-09-12 01:39:37 -0700

    updated

---
+++ my-app/src/components/seachbox.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Seachbox() {
+  return (
+    <>
+<input
+className='bg-gray-200 p-2 rounded-lg w-64'
+type="text" placeholder='City Name' />
+
+
+    </>
+  )
+}
